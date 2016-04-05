bef94aa83976f3a90f75c2b662a60a22795b1fa6
Marcus Vetter <marcus.vetter@live.com>
2016-04-05 13:15:47 +0200

    Refactored message styles

--- app/content-container/chat/message/message.ts
+++ app/content-container/chat/message/message.ts
@@ -3,21 +3,21 @@ import {Component, Input} from "angular2/core";
 @Component({
     selector: 'message',
     template: `
-        <div class="yoda-chat-msg-container">
-            <h2 class="yoda-chat-msg">{{ text }}</h2>
-            <p class="yoda-chat-msg-meta">{{ date | date:'medium' }} by {{ author }}</p>
+        <div class="message-container">
+            <h2 class="message">{{ text }}</h2>
+            <p class="message-meta">{{ date | date:'medium' }} by {{ author }}</p>
         </div>`,
     styles: [`
-        .yoda-chat-msg-container {
+        .message-container {
           margin-bottom: 2.5rem;
         }
         
-        .yoda-chat-msg {
+        .message {
           margin-bottom: .25rem;
           font-size: 1.5rem;
         }
         
-        .yoda-chat-msg-meta {
+        .message-meta {
           margin-bottom: 1.25rem;
           font-size: 1.2rem;
           color: #999;
