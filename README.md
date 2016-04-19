fed25b2a3fbef3ac3ef8e204bc21e119bbce31fb
Marcus Vetter <marcus.vetter@live.com>
2016-04-19 15:04:01 +0200

    Refactored backend server

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 * **_npm install -g webpack webpack-dev-server protractor_**
 
 ## How to build/run
-* **_npm run start_** for starting the dev server and running the application
+* **_npm run start-backend_** for starting the application backend
+* **_npm run start_** for starting the dev server and running the application frontend
 * **_npm run build_** for creating a bundle (prod)
 * **_protractor protractor.config.js_** for executing the end2end tests
---
+++ backend/server.js
@@ -0,0 +1,140 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+var router = express.Router();
+var convertFromYodaText = require('./convertFromYodaText');
+var convertToYodaText = require('./convertToYodaText');
+var debug = require('debug')('backend:server');
+var http = require('http');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+/**
+ *  MESSAGES
+ */
+var yodaMessages = [];
+var jediMessages = [];
+router.get('/messages', function(req, res) {
+    var mode = req.param('mode');
+    console.log('[GET /messages/' + mode + ']');
+
+    if (mode == '0') {
+        // YODA
+        res.send(yodaMessages);
+    } else {
+        // JEDI
+        res.send(jediMessages);
+    }
+});
+router.put('/messages', function (req, res) {
+    var message = req.body;
+    var mode = req.param('mode');
+    console.log('[PUT /messages/' + mode + ']: ' + JSON.stringify(message));
+    if (mode == '0') {
+        // YODA
+        yodaMessages.push(message);
+        var jediMessage = JSON.parse(JSON.stringify(message));
+        jediMessage.text = convertFromYodaText(jediMessage.text);
+        jediMessages.push(jediMessage);
+    } else {
+        // JEDI
+        jediMessages.push(message);
+        var yodaMessage = JSON.parse(JSON.stringify(message));
+        yodaMessage.text = convertToYodaText(yodaMessage.text);
+        yodaMessages.push(yodaMessage);
+    }
+    res.end();
+});
+router.delete('/messages', function (req, res) {
+    yodaMessages = [];
+    jediMessages = [];
+    res.end()
+});
+
+app.use('/', router);
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
+console.log('Backend server started successfully on port ' + port);
